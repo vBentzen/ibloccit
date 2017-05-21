44b0bbf86f50d286fd7ba721af7aae3e67e5cfba
Aske Bentzen <askebentzen@gmail.com>
2017-05-21 11:41:35 +0200

    fixing typo in comment test that made it fail

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-	let(:topic) { Topic.create!(name: name, description: description) }
-	let(:post) { topic.posts.create!(title: title, body: body) }
+  let(:topic) { Topic.create!(name: Faker::StarWars.character, description: Faker::Lorem.paragraph) }
+  let(:post) { topic.posts.create!(title: Faker::StarWars.character, body: Faker::Lorem.paragraph) }
 	let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
 	describe "attributes" do 
 		it "has a body attribute" do
-			expect(comment).to have_attributes(body: "Comment Body")
+			expect(comment).to have_attributes(body: "Comment Body", post: post)
 		end
 	end
 end
